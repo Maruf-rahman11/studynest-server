ac2cf03671ec8434ffb4e09f5b85b92d7e875589
Maruf-rahman11 <mrsohan34@gmail.com>
2025-09-17 23:46:53 +0600

    update

--- index.js
+++ index.js
@@ -158,8 +158,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -171,7 +171,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello sohan!')
 })
 
 app.listen(port, () => {
